56b81d15bfd1234dd0333d8677b0ed541cb8178f
HARSH-dev1708 <harshk0804@gmail.com>
2026-09-20 09:15:12 +0530

    Update UI text and test file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi Harsh, Welcome to React
         </a>
       </header>
     </div>
